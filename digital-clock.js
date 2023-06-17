2864366d0574999227f1b67efbdc38df3e757a3b
houssem <houssem.regaieg@etudiant-enit.utm.tn>
2023-06-17 11:35:31 +0100

    this is the last version of the project

---
+++ digital-clock.js
@@ -0,0 +1,20 @@
+let hours=document.getElementById('hours');
+let minutes=document.getElementById('minutes');
+let seconds=document.getElementById('seconds');
+console.log(hours.textContent);
+
+
+function rightTextContent(value){
+    return value<10? '0'+value :value;
+}
+
+function updatTime(){
+    const date=new Date();
+    hours.textContent= rightTextContent(date.getHours());
+    minutes.textContent=rightTextContent(date.getMinutes());
+    seconds.textContent=rightTextContent(date.getSeconds());
+
+}
+
+setInterval(updatTime,1000);
+
